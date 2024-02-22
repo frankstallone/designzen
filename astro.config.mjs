b1f611c60e8fb58fb982e8f2ec67683acc25b907
Frank Stallone <frank@designzen.co>
2024-02-22 16:58:05 -0500

    feat: ✨ Adding FAQs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://designzen.co',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), alpinejs()]
 });
