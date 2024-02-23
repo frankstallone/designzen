56fbdf8cd7dcef436416fac69f7e552bf6e496ed
Frank Stallone <frank@designzen.co>
2024-02-23 14:20:22 -0500

    config: ⚙<fe0f> Adding GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import alpinejs from '@astrojs/alpinejs';
 
-import alpinejs from "@astrojs/alpinejs";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://designzen.co',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), alpinejs()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    alpinejs(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
